6c16b2dc46710be4555f81a0dfb4c3e55cc553dc
dchen <dchen@ubuntu.(none)>
2014-03-06 23:48:46 -0500

    added initial tower defense py file

---
+++ tower_defense_game.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar  6 23:46:34 2014
+
+@author: dchen,gcoleman,mbocamazo
+"""
+import pygame
+from pygame.locals import *
+import random
+import math
+import time
+

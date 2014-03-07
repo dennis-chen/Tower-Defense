eeccdc7469679f149d7db9bd95a7a361a784310f
dchen <dchen@ubuntu.(none)>
2014-03-06 23:59:39 -0500

    added docstrings and class templates

--- tower_defense_game.py
+++ tower_defense_game.py
@@ -10,3 +10,32 @@ import random
 import math
 import time
 
+class TDModel:
+    """encodes the game state"""
+    def __init__(self):
+        
+
+class Grid:
+    """encodes positions of towers and path squares"""
+    def __init__(self):
+        
+
+class Creeps:
+    """encodes the state of a creep within the game"""
+    def __init__(self):
+        
+class Tower:
+    """encodes the state of a tower within the game"""
+    def __init__(self):
+        
+class Bullets:
+    """encodes the state of a bullet within the game"""
+    def __init__(self):
+        
+class Path:
+    """list of positions within the grid"""
+    def __init__(self):
+        
+class PyGameWindowView:
+    """renders TD model to game window"""
+    def __init__(self):

e3a9d4df6fc3cbff2fd93fd5208552f5a2a81485
dchen <dchen@ubuntu.(none)>
2014-03-08 22:13:50 -0500

    brah

--- tower_defense_game.py
+++ tower_defense_game.py
@@ -13,8 +13,8 @@ import numpy
 
 class TDModel:
     """encodes the game state"""
-    def __init__(self):
-        return
+    def __init__(self, tileGrid):
+        self.tileGrid = tileGrid
 
 class TileGrid:
     """encodes tower and path tiles"""
@@ -25,44 +25,58 @@ class TileGrid:
         x = randint(1,15)
         tiles[x][y] = start_tile
         path_list = []
+        path_list.append(self.return_center(x,y))
         for i in range(0,randint(3,5)):
             y-=1
             tile = PathTile()
-            path_list.append(tile)
             tiles[x][y] = tile
+        path_list.append(self.return_center(x,y))
         direction = random.choice([1,-1])
         for i in range(0,randint(0,min(x-1,16-x-1))): #avoid going out of the grid in the x direction 
             x-=direction
             tile = PathTile()
-            path_list.append(tile)
             tiles[x][y] = tile
+        path_list.append(self.return_center(x,y))
         for i in range(0,randint(3,5)):
             y-=1
             tile = PathTile()
-            path_list.append(tile)
             tiles[x][y] = tile
+        direction = random.choice([1,-1])
+        path_list.append(self.return_center(x,y))
         for i in range(0,randint(0,min(x-1,16-x-1))): #avoid going out of the grid in the x direction 
             x-=direction
             tile = PathTile()
-            path_list.append(tile)
             tiles[x][y] = tile
-        while y >= 0:
+        path_list.append(self.return_center(x,y))
+        while y > 0:
             y-=1
             tile = PathTile()
-            path_list.append(tile)
             tiles[x][y] = tile
+        path_list.append(self.return_center(x,y))
+        self.path_list = path_list
             
+    def return_center(self,x_in_grid,y_in_grid): #tiles are 40 by 40 pixels, grid is 640 by 640
+        return (x*40+20,y*40+20)
+        
+    def return_drawing_position(self,x_in_grid,y_in_grid):
+        return (x*40,y*40)
+        
+    def return_creep_path(self):
+        return self.path_list
         
         
-#class PathTile:
-#    
-#    def return_center_pos:    
-#    
-#class TowerTile:
-#    
-#    def__init__(self):
-#        self.level = 1
-#        self.speed = 1
+class PathTile:
+    def __init__(self,color):
+        self.color = color
+        
+                
+        
+class TowerTile:
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+        self.level = 1
+        self.speed = 1
 
 class Creeps:
     """encodes the state of a creep within the game"""
@@ -121,6 +135,7 @@ class PyGameWindowView:
     #reference
     def draw(self):
         self.screen.fill(pygame.Color(0,0,0))
+        
 #        for brick in self.model.bricks:
 #            pygame.draw.rect(self.screen, pygame.Color(brick.color[0], brick.color[1], brick.color[2]), pygame.Rect(brick.x, brick.y, brick.width, brick.height))
 #        pygame.draw.rect(self.screen, pygame.Color(self.model.paddle.color[0], self.model.paddle.color[1], self.model.paddle.color[2]), pygame.Rect(self.model.paddle.x, self.model.paddle.y, self.model.paddle.width, self.model.paddle.height))
@@ -138,11 +153,11 @@ class PyGameMouseController:
 
 if __name__ == '__main__':
     pygame.init()
-
-    size = (640,640)
+    tile_grid = TileGrid()
+    size = (640,690)
     screen = pygame.display.set_mode(size)
 
-    model = TDModel()
+    model = TDModel(tile_grid)
     view = PyGameWindowView(model,screen)
     controller = PyGameMouseController(model)
     running = True
